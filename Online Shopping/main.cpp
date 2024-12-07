77b083896bc3aefeb602204212f01a39e5c3e855
haytam raba <124938747+fauconraba@users.noreply.github.com>
2024-12-07 13:57:11 +0100

    Update main.cpp

--- Online Shopping/main.cpp
+++ Online Shopping/main.cpp
@@ -24,11 +24,6 @@ int main()
  string productName;
  bool buy=false;
 
-
- //interface
- // choices
- // more and more
-
  do
  {
   cout<<"  Welcome to the Online Shopping cart!"<<endl;
@@ -39,73 +34,73 @@ int main()
   cout<<"5. Checkout"<<endl;
   cout<<"0. Exit"<<endl;
   cout<<"Enter your choice:";
-  cin>>choice;
-  switch (choice)
-  {
-  case 1 :
-   cout<< " \n products available : \n";
-   for( i=0; i<products.size();i++)
-   {
-    cout<<i+1 <<". ";
-    products[i].display();
-   }break;
-
-  case 2:
-   cout<<"\n __________________________________ \n";
-   cout<<"enter the proDuct number";
-   cin>>productindex;
-   cout<<"add the quantity :";
-   cin>>quantity;
-   if( productindex > 0 && productindex <= products.size() )
-   {
-    if(quantity<products[productindex].getStock() && quantity >0 )
+   cin>>choice;
+    switch (choice)
     {
-     cart.addItem(products[productindex-1],quantity);
-     cout << "Added to cart!" << endl;
+     case 1 :
+        cout<< " \n products available : \n";
+      for( i=0; i<products.size();i++)
+      {
+         cout<<i+1 <<". ";
+         products[i].display();
+      }break;
+   
+     case 2:
+      cout<<"\n __________________________________ \n";
+      cout<<"enter the proDuct number";
+        cin>>productindex;
+      cout<<"add the quantity :";
+        cin>>quantity;
+      if( productindex > 0 && productindex <= products.size() )
+      {
+       if(quantity<products[productindex].getStock() && quantity >0 )
+       { // quantity disponible
+          cart.addItem(products[productindex-1],quantity);
+          cout << "Added to cart!" << endl;
+       }
+       else
+       { //quantity indisponible
+          cout<<"quantity invalid ! try again"<<endl;
+       }
+      }
+      else
+      {
+        cout<<"product number invalid , try again !" <<endl;
+      }
+      break;
+   
+     case 3:
+       cout<<"product name : " ;
+        cin>>productName;
+       cart.removeItem(productName);
+      break;
+   
+     case 4 :
+       cart.displayItems();
+      break;
+   
+     case 5:
+        cout<<"Total cost : ";
+        cout<<quantity*products[productindex].getPrice(); // total price = quantity * price 
+        cout<<" DH";
+      buy=true; //check if just a visite or buy something
+      choice = 0; //exit
+      break;
+   
+     case 0 :
+       if(buy)
+       {
+          cout<<"Thank you for shopping with us, Goodbye!"<<endl;
+       }
+       else
+       {
+          cout << "thank you for ur viste ,Goodbye!"<<endl;
+       }
+      break;
+     default :
+      cout << "Invalid choice. Please try again!" << endl;
+      break;
     }
-    else
-    {
-     cout<<"quantity invalid ! try again"<<endl;
-    }
-   }
-   else
-   {
-    cout<<"product number invalid , try again !" <<endl;
-   }
-   break;
-
-  case 3:
-    cout<<"product name : " ;
-    cin>>productName;
-    cart.removeItem(productName);
-   break;
-
-  case 4 :
-    cart.displayItems();
-   break;
-
-  case 5:
-     cout<<"Total cost : ";
-     cout<<quantity*products[productindex].getPrice();
-     cout<<" DH";
-   buy=true;
-   choice = 0; //exuit
-   break;
-
-  case 0 :
-    if(buy)
-    {
-       cout<<"Thank you for shopping with us, Goodbye!"<<endl;
-    }
-    else
-    {
-       cout << "thank you for ur viste ,Goodbye!"<<endl;
-    }
-   break;
-  default :
-   cout << "Invalid choice. Please try again!" << endl;
-   break;
-  }
   }while(choice != 0);
 
 
